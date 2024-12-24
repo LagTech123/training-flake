7e8a6692fbb2fb18b6191fec7ebf9ee8241804b1
lactose <lactose@allthingslinux.com>
2024-12-24 05:31:35 +0000

    set up boilerplate flake

---
+++ README.md
@@ -0,0 +1,2 @@
+# Testing repo
+This flake is for training for Monarch team members
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Flake for training dev team";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          packages = {
+            hello = pkgs.callPackage ./hello.nix { };
+          };
+
+          formatter =
+            let
+              treefmt-eval = inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+            in
+            treefmt-eval.config.build.wrapper;
+        };
+    };
+}
